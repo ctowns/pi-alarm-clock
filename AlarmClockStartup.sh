6521b5963e17ebdc16592f83d0afec25c78d8f3c
ctowns <cooperextra@gmail.com>
2017-11-19 17:05:25 -0500

    Add files via upload

--- AlarmClockStartup.sh
+++ AlarmClockStartup.sh
@@ -1,12 +1,13 @@
+
 #! /bin/sh
 #start alarm clock
 
-#must change to AlarmClock directory to get SevenSegment package
 #start clock display service
 cd /home/pi/AlarmClock/ && python AlarmClock_WeatherButton.py&
 
 #start Alexa
-#cd ~/Desktop/alexa-avs-sample-app/samples/companionService && npm start&
-#cd ~/Desktop/alexa-avs-sample-app/samples/javaclient && mvn exec:exec&
+cd Desktop/alexa-avs-sample-app/samples/companionService && npm start&
+cd Desktop/alexa-avs-sample-app/samples/javaclient && mvn exec:exec&
 
+echo date > AlarmClockStart.txt
 echo "Alarm Clock Started" > /tmp/AlarmClockStart.txt
